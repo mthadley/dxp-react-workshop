c9ffd17a942662cf4af2f49d7dcf9bc3280eed5a
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2016-09-06 10:05:58 -0700

    Comment out entry.js

--- todo/todo-web/src/main/resources/META-INF/resources/js/src/entry.js
+++ todo/todo-web/src/main/resources/META-INF/resources/js/src/entry.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import {render} from 'react-dom';
 
-import App from './components/App';
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import {render} from 'react-dom';
+//
+// import App from './components/App';
 // import Todo from './components/Todo'; // Final Product
-
-window.App = (node) => {
-	render(
-		<App />,
-		node
-	);
-}
+//
+// window.App = (node) => {
+// 	render(
+// 		<App />,
+// 		node
+// 	);
+// }
